69a6902a82cd3f242022bdca1c296fd654f2e1e7
TupiRo <zarate.ro@gmail.com>
2024-05-30 19:51:26 -0400

    Base CRUD for Pet endpoint
    Base CRUD for Pet endpoint
    Generic URLs

---
+++ cypress/e2e/pet_crud_suite.cy.js
@@ -0,0 +1,70 @@
+import * as endpoints from '../support/endpoints'
+
+describe('PET: CRUD TCs for Pet objet', () => {
+    it('GET Pet by ID', () => {
+        cy.request({ 
+            method : 'GET',
+            url : Cypress.env('BASE_URL') + endpoints.GET_PET_BY_ID
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.name).to.eq('Cat 1')
+
+        })
+    })
+
+    it('POST Pet', () => {
+        let payload = {
+            "id": 11,
+            "name": "TestDog2",
+            "category": {
+              "id": 1,
+              "name": "Dogs"
+            },
+            "status": "available"
+          }
+        cy.request({ 
+
+            method : 'POST',
+            url : Cypress.env('BASE_URL') + endpoints.POST_PET,
+            body : payload
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.name).to.eq(payload.name)
+
+        })
+    })
+
+    it('PUT Pet', () => {
+        let payload = {
+            "id": 11,
+            "name": "TestDog2Updated",
+            "category": {
+              "id": 1,
+              "name": "Dogs"
+            },
+            "status": "available"
+          }
+        cy.request({ 
+
+            method : 'PUT',
+            url : Cypress.env('BASE_URL') + endpoints.POST_PET,
+            body : payload
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.name).to.eq(payload.name)
+
+        })
+    })
+
+    it('Delete a pet by ID', () => {
+        cy.request({ 
+
+            method : 'DELETE',
+            url : Cypress.env('BASE_URL') + endpoints.GET_PET_BY_ID
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.message).to.eq('Pet deleted')
+
+        })
+    })
+})
---
+++ cypress/support/endpoints.js
@@ -0,0 +1,2 @@
+export const GET_PET_BY_ID = 'pet/11'
+export const POST_PET = 'pet'
